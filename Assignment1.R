1b4c24db50f93c746768b592f3bc59ea07319dca
kushwantkaur <71961120+kushwantkaur@users.noreply.github.com>
2020-09-29 13:10:29 +0530

    Add files via upload

---
+++ Assignment1.R
@@ -0,0 +1,23 @@
+CacheMatrix1 <- function(x = matrix()){
+  inv <- NULL
+  set <- function(y)
+  {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function(){x}
+  setInverse <- function(inverse) {inv << -inverse}
+  getInverse <- function() {inv}
+  list(set = set, get = get, setInverse =setInverse, getInverse = getInverse)
+}
+cachesolve <- function(x, ...){
+  inv <- x$getInverse()
+  if(!is.null(inv)){
+    message("succes")
+    returned(inv)
+  }
+  mat <- x$get()
+  inv <- solve(inv)
+  inv
+}
+}
